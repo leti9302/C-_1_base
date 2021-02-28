342b1afcd2cbd7297629c4d89daff93ea8c3f9bc
Semenov Alexey <58954003+leti9302@users.noreply.github.com>
2021-02-28 19:41:15 +0300

    Mine

--- C_1_base/C_1_base/YourTasksHere.cs
+++ C_1_base/C_1_base/YourTasksHere.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +17,7 @@ namespace C_1_base
 
         public static double SquerOfTrungle(uint a, uint b)
         {
-            throw new NotImplementedException();
+            return (double)(0.5*a*b);
         }
         #endregion
 
@@ -31,9 +31,138 @@ namespace C_1_base
 
         public static bool isTrungle(uint a, uint b, uint c)
         {
-            throw new NotImplementedException();
+            if ((a <= b + c) && (b <= a + c) && (c <= a + b)) return true;
+            else return false;
         }
         #endregion
+
+
+        #region Задача 3
+        /*
+         * На вход подается два числа, если числа имеют одинаковые знаки, необходимо вывести сумму, иначе разность, взятую по модулю
+        */
+
+        public static int Arifmetika(int a, int b)
+        {
+            if (a * b >= 0) return a + b;
+            else return Math.Abs(Math.Abs(a) - Math.Abs(b));
+        }
+        #endregion
+
+
+        #region Задача 4
+        /*
+         * На вход подается число, необходимо найти и вернуть факториал этого числа (гарантируется, что входное число больше или равно 1)
+        */
+
+        public static int Fuc(int a)
+        {
+            if (a > 1) return a * Fuc(a - 1);
+            else return 1;
+        }
+        #endregion
+
+
+        #region Задача 5
+        /*
+         * На вход подается число n, необходимо создать массив длинной n и проинициализровать его значениями n, n-1 ... 1 (гарантируется, что n >= 1)
+        */
+
+        public static int[] ArrayCreate(int n)
+        {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) array[i] = n-i;
+            return array;
+        }
+        #endregion
+
+
+        #region Задача 6
+        /*
+         * На вход подается массив чисел, необходимо вернуть индекс первого вхождения наибольшего элемента массива
+        */
+
+        public static int MaxSearch(int[] array)
+        {
+            int max = array[0];
+            int maxIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+        #endregion
+
+
+        #region Задача 7
+        /*
+         * На вход подается массив чисел, необходимо вернуть его полную копию
+        */
+
+        public static int[] ArrayCopy(int[] array)
+        {
+            int[] copy = new int[array.Length];
+            for(int i = 0; i < array.Length; i++)
+            {
+                copy[i] = array[i];
+            }
+            return copy;
+        }
+        #endregion
+
+
+        #region Задача 8
+        /*
+         * Реализуйте рекурсивную функцию нахождения чисел Фибоначи (a(n) = a(n-1) + a(n-2), где a(1) - 1, a(2) - 2)
+        */
+
+        public static int Fibonachi(int n)
+        {
+            if (n == 1 || n == 2) return n;
+            else return (Fibonachi(n - 1) + Fibonachi(n - 2));
+        }
+        #endregion
+
+
+        #region Задача 9
+        /*SS
+         * На вход подается массив, необходимо найти наибольшее значение в нем (рекомендуется использовать функцию из задания 6),
+         * а так же вернуть полную копию входного массива с помощью модификатора out (рекомендуется использовать функцию из задания 7)
+        */
+
+        public static int HardArray(int[] intput, out int[] output)
+        {
+            output = new int[intput.Length];
+            output = ArrayCopy(intput);
+            return intput[MaxSearch(intput)];
+        }
+        #endregion
+
+
+        #region Задача 10
+        /*
+         * Вы создаете боевую систему для компьютерной игры
+         * Вам необходимо написать метод, который расчитывает результаты боя
+         * На каждом ходу игроки наносят урон противнику равный их силе
+         * Бой заканчивается, когда у одного из игроков (или у обоих) здоровье опуститься до 0 или ниже
+        */
+
+        public static void Fight(ref int health1, int power1, ref int health2, int power2)
+        {
+            while (health1 > 0 && health2 > 0)
+            {
+                health1 -= power2;
+                health2 -= power1;
+            }
+        }
+        #endregion
+    }
+}
 
 
         #region Задача 3
